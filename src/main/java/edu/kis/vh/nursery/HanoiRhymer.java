dc3ff2c8a02910487907632ff542b9c849a20ca0
Michał Kawecki Politechnika <255621@edu.p.lodz.pl>
2026-04-11 18:11:26 +0200

    17. dokumentacja dla HanoiRhymer.java

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,35 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer rozszerza zachowanie klasy DefaultCountingOutRhymer poprzez dodanie mechanizmu zliczania odrzuconych elementów.
+ * Element zostaje odrzucony w sytuacji, gdy:
+ * stos nie jest pusty (callCheck() == false) oraz wartość wejściowa jest większa od aktualnego wierzchołka stosu (peekaboo())
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Licznik odrzuconych wartości (niezapisanych na stosie).
+     */
     private int totalRejected = 0;
 
+    /**
+     * Zwraca liczbę elementów, które zostały odrzucone podczas próby dodania na stos.
+     *
+     * @return liczba odrzuconych elementów
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Dodaje element na stos.
+     * Jeżeli stos nie jest pusty i nowa wartość jest większa od
+     * aktualnego wierzchołka stosu, element zostaje odrzucony i
+     * zwiększany jest licznik odrzuceń.
+     * W przeciwnym wypadku element zostaje dodany na stos.
+     *
+     * @param in wartość dodawana do stosu
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
