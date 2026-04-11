beebf59c52e4aaf2c2fca5bedeb275e2f501c37f
Michał Kawecki Politechnika <255621@edu.p.lodz.pl>
2026-04-11 16:04:07 +0200

    14. Usunieto Node.java, dodano klase Node jako wewnetrzna dla intlinkedlist

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -5,6 +5,38 @@ public class IntLinkedList {
     private static final int ERROR = -1;
     private Node last;
 
+    private static class Node {
+
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        Node(int i) {
+            value = i;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        Node getPrev() {
+            return prev;
+        }
+
+        void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+
     public void push(int i) {
         if (last == null)
             last = new Node(i);
